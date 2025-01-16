927af34ea4842f350b272e99be1e7392c871f570
ankitsharma003 <146851044+ankitsharma003@users.noreply.github.com>
2025-01-16 10:15:55 +0530

    add new project

--- src/assets/All_projectsdata.js
+++ src/assets/All_projectsdata.js
@@ -12,63 +12,75 @@ import qr_phone from "../assets/qr-phone.png";
 import to_do_web from "../assets/to-do-web.png";
 import to_do_tab from "../assets/to-do-tab.png";
 import password_web from "../assets/password-web.png";
+import TechSchool_img from "../assets/trechSchool_img.png";
+import TechSchool_img_2 from "../assets/techScool_img2.png";
+import TechSchool_img_3 from "../assets/texhSchool_img3.png";
 
 export const All_projectsdata = [
   {
-
     id: 1,
-    name: "Akshay Sweets",
-    image: akshay_sweets_web  ,
-    img1: akshay_sweets_tab  ,
-    img2: akshay_sweets_phone  ,
+    name: "TechSchool",
+    image: TechSchool_img,
+    img1: TechSchool_img_2,
+    img2: TechSchool_img_3,
     description:
-      "I am thrilled to present Akshay Sweets, a delightful e-commerce platform dedicated to bringing the joy of exceptional sweets to the digital world. This project is a harmonious blend of exquisite confections, user-friendly design, and seamless functionality.",
-    link: "https://akshay-sweets-frontend.onrender.com/",
-    gitlink:"https://github.com/ankitsharma003/akshay-sweets"
+      "TechSchool is an interactive platform designed to showcase and manage technical courses. Built with ReactJS, it offers a smooth and responsive user experience, allowing users to browse courses, search for topics, and engage with a modern UI.",
+    link: "https://teach-school.vercel.app/",
+    gitlink: "https://github.com/ankitsharma003/TeachSchool",
   },
   {
-
     id: 2,
-    name: "Weather",
-    image: weather_web  ,
-    img1:  weather_tab ,
-    img2: weather_phone ,
+    name: "Akshay Sweets",
+    image: akshay_sweets_web,
+    img1: akshay_sweets_tab,
+    img2: akshay_sweets_phone,
     description:
-      "I am proud to showcase my Weather App project, a dynamic and user-friendly application designed to provide real-time weather information effortlessly.this app seamlessly integrates with weather APIs to deliver up-to-date details, including temperature, humidity, wind speed, and more. The responsive design ensures a consistent and enjoyable experience across various devices.",
-    link: "https://ankitsharma003.github.io/Weather/",
-    gitlink:"https://github.com/ankitsharma003/Weather"
-  },  {
-
+      "Akshay Sweets is a delightful e-commerce platform dedicated to bringing exceptional sweets to the digital world. This project combines exquisite confections with a user-friendly design and seamless functionality.",
+    link: "https://akshay-sweets-frontend.onrender.com/",
+    gitlink: "https://github.com/ankitsharma003/akshay-sweets",
+  },
+  {
     id: 3,
-    name: "QR Generator ",
-    image: qr_web  ,
-    img1: qr_tab  ,
-    img2: qr_phone ,
+    name: "Weather",
+    image: weather_web,
+    img1: weather_tab,
+    img2: weather_phone,
     description:
-      "I am excited to present my QR Code Generator project, a versatile and user-friendly tool that empowers users to create QR codes effortlessly. This project provides a simple yet powerful interface where users can input various types of data, such as URLs, text, or contact information, and instantly generate QR codes.",
-    link: "https://ankitsharma003.github.io/QR-GENERATOR/",
-    gitlink:"https://github.com/ankitsharma003/QR-GENERATOR"
-  },  {
-
+      "Weather App provides real-time weather information with a seamless user experience. Integrated with weather APIs, it displays temperature, humidity, wind speed, and more, with a responsive design for all devices.",
+    link: "https://ankitsharma003.github.io/Weather/",
+    gitlink: "https://github.com/ankitsharma003/Weather",
+  },
+  {
     id: 4,
+    name: "QR Generator",
+    image: qr_web,
+    img1: qr_tab,
+    img2: qr_phone,
+    description:
+      "QR Code Generator allows users to easily create QR codes for URLs, text, or contact information. The app provides a simple interface for generating custom QR codes with ease.",
+    link: "https://ankitsharma003.github.io/QR-GENERATOR/",
+    gitlink: "https://github.com/ankitsharma003/QR-GENERATOR",
+  },
+  {
+    id: 5,
     name: "To Do list",
-    image: to_do_web  ,
-    img1: to_do_tab ,
+    image: to_do_web,
+    img1: to_do_tab,
     img2: "",
     description:
-      "Introducing my To-Do List App, a productivity tool that streamlines task management and enhances organization. This application is thoughtfully designed to simplify the user's daily workflow, allowing for the creation, tracking, and completion of tasks with efficiency. With an intuitive user interface, users can easily add, ensuring a seamless planning experience.",
+      "To-Do List App streamlines task management and enhances productivity. With an intuitive interface, users can efficiently create, track, and complete tasks, making it easy to stay organized.",
     link: "https://ankitsharma003.github.io/To-Do-list/",
-    gitlink:"https://github.com/ankitsharma003/To-Do-list"
-  },{
-
-    id: 5 ,
+    gitlink: "https://github.com/ankitsharma003/To-Do-list",
+  },
+  {
+    id: 6,
     name: "Password Generator",
-    image: password_web  ,
-    img1: "" ,
+    image: password_web,
+    img1: "",
     img2: "",
     description:
-      "I am thrilled to showcase my Password Generator project, a robust and secure tool designed to enhance online security. This application empowers users to create strong and unique passwords with ease, helping to fortify their digital accounts against unauthorized access. The Password Generator utilizes advanced algorithms to generate random and complex password combinations, ensuring a high level of security. The user-friendly interface allows customization of password length, and other parameters to meet specific security requirements. ",
+      "Password Generator helps users create strong and secure passwords. With customizable options, it generates random, complex password combinations to ensure a high level of security for online accounts.",
     link: "https://ankitsharma003.github.io/RANDOM-PASSWORD-GENERATORE/",
-    gitlink:"https://github.com/ankitsharma003/RANDOM-PASSWORD-GENERATORE"
+    gitlink: "https://github.com/ankitsharma003/RANDOM-PASSWORD-GENERATORE",
   },
 ];
